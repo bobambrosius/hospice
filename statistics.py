68427e72cccb3ef428224a59128311a8f8d2acc7
bobambrosius <bob.ambrosius@gmail.com>
2023-03-10 12:12:49 +0100

    Worked on statistics.py

--- statistics.py
+++ statistics.py
@@ -1,6 +1,6 @@
 import init_volunteers
 
-def count_shifts_per_weeks(all_persons):
+def count_per_shifts_per_weeks(all_persons):
     caretakers = [ p for p in all_persons if p.service == 'verzorger']
     generalist = [ p for p in all_persons if p.service == 'algemeen']
     caretaker_counts = {}
@@ -17,14 +17,15 @@ def count_shifts_per_weeks(all_persons):
              if p.shifts_per_weeks.shifts == shifts
              and p.shifts_per_weeks.per_weeks == weeks
             ])
-    return caretaker_counts, generalist_counts
+    print(f'gewicht verzorgers: {caretaker_counts}, \ngewicht algemenen: {generalist_counts}')
 
-def show_days_and_shifts_count(all_persons):
+def days_and_shifts_count(all_persons):
     """Sum the count_of_shifts for each person"""
 
     count = {}
-    innercount = {}
-    for service in ('verzorger', 'algemeen'):
+    services = ('verzorger', 'algemeen')
+    for service in services:
+        innercount = {}
         for id, prs in enumerate(prs for prs in all_persons
                               if prs.service == service):
             length = 0
@@ -34,17 +35,30 @@ def show_days_and_shifts_count(all_persons):
             # Do for each person 28 -/- count of shifts
             # so we can have like 20/28 as an indication
             # of the persons capacity for the planning.
-            innercount[id + 1] = 28 - length
+            innercount[id + 1] = length
+            
+            """innercount now has 's' of the numerator of the first term
+            in the formula: ((28-s)/28) * (1/28) * weight.
+            Explanation:
+            1/28 is the planningcapacity that a person has 
+            if she is available the whole week.
+            The weight indicates shifts_per_weeks.
+            If a person does not work on 7 shifts per week, then the
+            planningcapacity = (28-7)/28 = 21/28 = 3/4 of 1/28 * weight.
+            If a person does not work on 0 shifts per week, then the
+            planningcapacity = (28-0)/28 = 28/28 = 1 of 1/28 * weight.
+            """
         count[service] = innercount
-        print(f'{service}: {count[service]}')
-    # Now get avarage, median and modus.
-    
-    #indication = [ round(val/28, 2) for val in count_caretaker[service].values() ]
-    #print(indication)    
+        pass
+    return count['verzorger'], count['algemeen']    
 
 
 if __name__ == '__main__':
     input_filename = "vrijwilligers-2023-kw2.csv"
-    volunteers = init_volunteers.Volunteer(input_filename)
-    cc, gc = count_shifts_per_weeks(volunteers.persons)
-    show_days_and_shifts_count(volunteers.persons)
+    volunteers = init_volunteers.Volunteers(input_filename)
+    print()
+    count_per_shifts_per_weeks(volunteers.persons)
+    print()
+    verzorgers_dasc, algemenen_dasc = days_and_shifts_count(volunteers.persons)
+    print(f'Aantal shifts niet van verzorgers: {verzorgers_dasc}')
+    print(f'Aantal shifts niet van algemenen: {algemenen_dasc}')
